30f150e36fbdb599d9d7897aa62cedb9f9b7dfcf
abhilash4252 <abhilash4252@gmail.com>
2015-11-02 12:21:25 +0530

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,11 @@
 module SessionsHelper
-		def	log_in(user)
-			session[:user_id]	=	user.id
+	def	log_in(user)
+		session[:user_id]	=	user.id
 	end
-		def	current_user
-				if	(user_id	=	session[:user_id])
+	def	current_user
+		if (user_id	=	session[:user_id])
 			@current_user	||=	User.find_by(id:	user_id)
-	elsif	(user_id	=	cookies.signed[:user_id])
+	    elsif (user_id	=	cookies.signed[:user_id])
 			user	=	User.find_by(id:	user_id)
 			if	user	&&	user.authenticated?(cookies[:remember_token])
 					log_in	user
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Example	User",
+					email:	"example@gmail.com",
+					password: "1201357",
+					password_confirmation:	"1201357",
+					admin: true)
+99.times do	|n|
+		name =	Faker::Name.name
+		email	= "example-#{n+1}@gmail.com"
+		password = "password"
+		User.create!(name: name,
+					 email:	email,
+					 password: password,
+					 password_confirmation:	password)
+end
