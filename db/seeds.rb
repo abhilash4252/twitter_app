e4c7fd01bf8b274ff865a93353a9fdfc35b470da
abhilash4252 <abhilash4252@gmail.com>
2015-11-02 18:42:38 +0530

    microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ User.create!(name: "Example	User",
 					admin: true,
 					activated: true,
 					activated_at: Time.zone.now)
-99.times do	|n|
+9.times do	|n|
 		name =	Faker::Name.name
 		email	= "example-#{n+1}@gmail.com"
 		password = "password"
@@ -22,4 +22,10 @@ User.create!(name: "Example	User",
 					 password_confirmation:	password,
 					 activated: true,
 					 activated_at: Time.zone.now)
+users	=	User.order(:created_at).take(6)
+5.times do
+	content	=	Faker::Lorem.sentence(5)
+	users.each { |user|	user.microposts.create!(content: content) }
+end
+
 end
